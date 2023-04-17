3833a8589bac9bbbbf6d0861d3b9840db422f91b
Jai_Jayathilak <jaijayathilak@gmail.com>
2023-04-17 13:50:27 +0530

    todo item initialized

---
+++ src/Component/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoApp=()=>{
+    return(
+        <div class="ui action input">
+            <input type="text" placeholder="Enter a value" />
+            <button class="ui button">Add item</button>
+        </div>
+    )
+}
+
+export default TodoApp;

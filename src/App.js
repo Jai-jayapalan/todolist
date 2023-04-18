e19c64b942658ab5a0c1f55ec3a9de401878b912
Jai_Jayathilak <jaijayathilak@gmail.com>
2023-04-18 11:50:34 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TodoApp from './Component/Todo';
 function App() {
   return (
     <div className="App">
-      <h1>welcome</h1>
+      <h1>ToDoApp</h1>
       <TodoApp />
     </div>
   );
--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TodoApp=()=>{
-    return(
-        <div class="ui action input">
-            <input type="text" placeholder="Enter a value" />
-            <button class="ui button">Add item</button>
-        </div>
-    )
-}
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [editingId, setEditingId] = useState(null);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (editingId !== null) {
+      const updatedTodos = todos.map((todo) => {
+        if (todo.id === editingId) {
+          return { ...todo, text:inputValue };
+        }
+        return todo;
+      });
+
+      setTodos(updatedTodos);
+      setInputValue("");
+      setEditingId(null);
+    } else {
+      const newTodo = { id: Date.now(), text:inputValue };
+      setTodos([...todos, newTodo]);
+      setInputValue("");
+    }
+  };
+
+  const handleEditTodo = (id, text) => {
+    setInputValue(text);
+    setEditingId(id);
+  };
+
+  const handleRemoveTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <div className="ui action input">
+        <input type="text" placeholder="Enter a value" value={inputValue}
+            onChange={handleInputChange}
+        />
+        <button className="ui primary button" onClick={handleAddTodo}>
+            {editingId !== null ? "Update" : "Add"} Todo
+        </button>
+      </div>
+      <ul>
+        {
+        todos.map((todo) => (
+          <li key={todo.id} style={{listStyleType:'none'}}>
+            {todo.text}
+            <button className="ui blue button" onClick={() => handleEditTodo(todo.id, todo.text)}>Edit</button>
+            <button className="ui red button" onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default TodoApp;
